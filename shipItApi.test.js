bac69ccaad4567bdd642b17dffda295ddd957cdc
ross-brown <rbrown_98@yahoo.com>
2023-09-19 18:48:53 -0500

    add mocking to tests

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,20 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const fetchMock = require("fetch-mock");
+const { shipProduct, SHIPIT_SHIP_URL } = require("./shipItApi");
 
 
 test("shipProduct", async function () {
+  fetchMock.post(SHIPIT_SHIP_URL, {
+    body: {
+      receipt: {
+        shipId: 1234
+      }
+    },
+    status: 200
+  });
+
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +22,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(1234);
 });
